2c8d575035413dcadb43f36b0f17fe97926e5c08
Dijalma Duarte Fleitas <141293469+dani10014@users.noreply.github.com>
2025-11-05 22:02:56 -0400

    Add files via upload

--- viagens.js
+++ viagens.js
@@ -1,16 +1,5 @@
-window.onload = function(){
-        const moveConteudo = document.querySelector('.container-mae-todos');
-        const btnHome = document.querySelector(".btn-home"); 
-        moveConteudo.style.transform = "translateY(0)";
-        
-    btnHome.addEventListener("click", function(event){
-        event.preventDefault(); 
-
-        moveConteudo.style.transform = "translateY(100%)"; 
-
-        setTimeout(function() {
-            window.location.href = btnHome.href;
-        }, 500);
-    });
-
-};
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.querySelector('.container-mae-todos');
+    
+    setTimeout(() => container.style.transform = 'translateY(0)', 10);
+});
