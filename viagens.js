6f844cfa39f4652336363a07220f414780f69e08
Dijalma Duarte Fleitas <141293469+dani10014@users.noreply.github.com>
2025-11-06 08:28:26 -0400

    Corrigindo problema com scroll

--- viagens.js
+++ viagens.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const container = document.querySelector('.container-mae-todos');
     
-    setTimeout(() => container.style.transform = 'translateY(0)', 10);
+    setTimeout(() => container.style.transform = 'translateY(0)', 200);
 });
